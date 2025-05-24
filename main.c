6a88c5f17e3764e463ab4d85cf92372a75c18762
jockebengtsson@outlook.com <jockebengtsson@outlook.com>
2025-05-24 02:12:03 +0200

    notfinish

--- main.c
+++ main.c
@@ -11,24 +11,24 @@ int main(){
     int j = 0;
     int i = 0;
     int prov[5][13];
-    char person[5][10];
+    char person[5][11];
     int results[5];
     float result[5];                                               // hitta högsta värdet i results, printa namn på högsta
     int total = 0;
 
+
     for (j = 0; j < 5; j++){
-        scanf("%9s", person[j]);
+        scanf("%9s\0", person[j]);
         for (i = 0; i < 13; i++){
             scanf("%d", &prov[j][i]);
-            results[j] += prov[j][i];
-            total += prov[j][i];}
-        // printf("\nthis is prov %s, result: %d total: %d ", person[j], results[j], total);
-        result[j] = (float)results[j] / 13;}
-
-    float float_total = (float)total / 65;                                // ändra till float
-    // printf("\n%.2f", float_total);
-    float test = result[0];
-
+                  results[j] += prov[j][i];
+                  total += prov[j][i];}
+             //     printf("\nthis is prov %s, result: %d total: %d ", person[j], results[j], total);       
+                    result[j] = (float)results[j] / 13;
+      }
+        float float_total = (float)total / 65;                                // ändra till float
+       // printf("\n%.2f", float_total);
+        float test = result[0];
     for(int a=1; a<5; a++){                                              //kolla efter högsta värdet
        if(test < result[a]){                                             
          test = result[a];
@@ -63,25 +63,39 @@ int main(){
 // printf("%s", need_help[0][10]);
 // printf("%s", need_help[1][10]);
     if (test == result[0]){
-        printf("%s\n", person[0]);}
-      if (test == result[1]){
-          printf("%s\n", person[1]);}    
-        if (test == result[2]){
-            printf("%s\n", person[2]);}
-          if (test == result[3]){
-            printf("%s\n", person[3]);}
-              if (test == result[4]){
-                printf("%s\n", person[4]);}
-    if (float_total > result[0]){
-        printf("%s\n", person[0]);}
-      if (float_total > result[1]){
-        printf("%s\n", person[1]);}
-          if (float_total > result[2]){
-            printf("%s\n", person[2]);}
-              if (float_total > result[3]){
-                printf("Bob\n", person[3]);}
-                  if (float_total > result[4]){
-                    printf("%s\n", person[4]);}
+        printf("%s\n", person[0]);
+    } 
+    if (test == result[1]){
+        printf("%s\n", person[1]);
+    }    
+    if (test == result[2]){
+        printf("%s\n", person[2]);
+    }
+    if (test == result[3]){
+            printf("%s\n", person[3]);
+    }
+    if (test == result[4]){
+                printf("%s\n", person[4]);
+    }
 
-      return 0 ;
+    if (float_total > result[0]){
+        printf("%s\n", person[0]);
+    }
+    if (float_total > result[1]){
+        printf("%s\n", person[1]);
+    }
+    if (float_total > result[2]){
+        printf("%c", person[2][0] -32);
+        for (int k = 1; k < 5; k++){
+            printf("%c", person[2][k]);
+        }
+    }
+    if (float_total > result[3]){
+       printf("\n%s\n", person[3]);
+    }
+    if (float_total > result[4]){
+       printf("%s\n", person[4]);
+    }
+
+    return 0 ;
 }
